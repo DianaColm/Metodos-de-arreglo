1640ae1de9f55901ff1dda198d2c60e3d60f8cfb
DianaColm <dcolmenerog@yahoo.com.mx>
2018-02-21 23:07:10 -0600

    subiendo ejercicios callback, metodos de arreglo

---
+++ js/app.js
@@ -0,0 +1,7 @@
+//No se puede:
+//Usar el método .forEach , .map , .reduce, filter
+
+//1 ) Crea tu propia función forEach que funcione igual a Array.forEach. Algo asi:
+function miForEach(arreglo, callback){
+		// cpompleta aqui
+	}
